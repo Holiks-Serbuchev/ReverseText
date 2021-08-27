997edbad0dfd27a6d01e63a76399e6e332401848
Holiks-Serbuchev <dr.serbuchev@mail.ru>
2021-08-27 11:29:44 +0300

    Default

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+            while (text.Length > 100)
+            {
+                Console.WriteLine("Введите текст меньше 100 символов:");
+                text = Console.ReadLine();
+            }
+            Console.WriteLine(ReverseString(text, String.Empty));
+            Console.ReadLine();
+        }
+        public static string ReverseString(string originalText, string endText)
+        {
+            if (originalText.Length > endText.Length)
+            {
+                endText += originalText[originalText.Length - endText.Length - 1];
+                return ReverseString(originalText, endText);
+            }
+            else
+                return endText;
+        }
+    }
+}
